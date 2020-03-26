5aae89136032714602ac2fdd45a9199f730f3f5d
SUPEZ <cyberflower999@gmail.com>
2020-03-26 15:37:59 +0300

    version 0.1

---
+++ venv/AutoNMAP.py
@@ -0,0 +1,40 @@
+import sys
+import os
+import nmap
+
+if __name__ == '__main__':
+
+    # Get command arguments.
+    if len(sys.argv) != 3:
+        print("./AutoNMAP.py <<IP ADDRESS>> <<PORT RANGE>>")
+        sys.exit(0)
+    iptarget=sys.argv[1]
+    rgports=sys.argv[2]
+
+    print("---------" * 6)
+    ptint("     SCANNING THE TARGET" + iptarget)
+    print("---------" * 6)
+
+    # Initialization of Nmap
+    try:
+        nm = nmap.PortScanner()
+        nm.scan(iptarget,rgports)
+    except nmap.PortScannerError:
+        print('Nmap not found', sys.exc_info()[0])
+        sys.exit(0)
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        sys.exit(0)
+    # Write result of scanning
+    for host in nm.all_hosts():
+        print("Host: %s (%s)" % (host,iptarget))
+        print("State: %s" % nm[host].state())
+
+        for proto in nm[host].all_protocols():
+            print("---------" * 6)
+            print("protocol : %s" % proto)
+
+            ports=nm[host][proto].keys()
+            ports.sort()
+            for port in ports:
+                print("Port : %s \t State : %s " %(port[host][proto][port]['State']))
