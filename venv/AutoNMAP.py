ae3fe909a3e9e8502da5b55c29997b5d421faa1b
SUPEZ <cyberflower999@gmail.com>
2020-03-26 18:25:01 +0300

    version 0.1.8

--- venv/AutoNMAP.py
+++ venv/AutoNMAP.py
@@ -19,7 +19,7 @@ def main():
     # Initialization of Nmap
     try:
         nm = nmap.PortScanner()
-        nm.scan(iptarget, rgports, -T5 - Pn - sV - sT)
+        nm.scan(iptarget, rgports)
     except nmap.PortScannerError:
         print('Nmap not found', sys.exc_info()[0])
     except:
